18a33b595cd107c121bb8e920cbce552ad751329
Myo Thet <mr.myothet@gmail.com>
2024-10-31 14:51:45 +0700

    Add CityDto and CitiesDataStore

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -2,15 +2,19 @@
 
 namespace CityInfo.API.Controllers;
 
-[Route("api/[Controller]")]
+[Route("api/cities")]
 [ApiController]
 public class CitiesController : ControllerBase
 {
     [HttpGet]
     public JsonResult GetCities()
     {
-        return new JsonResult(
-            new List<object> { new { id = 1, Name = "New York" }, new { id = 2, Name = "Queens" } }
-        );
+        return new JsonResult(CitiesDataStore.Current.Cities);
+    }
+
+    [HttpGet("{id}")]
+    public JsonResult GetCity(int id)
+    {
+        return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
     }
 }

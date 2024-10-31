b39a55ac8ad1bd526bc8b0900245759f535af66a
Myo Thet <mr.myothet@gmail.com>
2024-10-31 17:30:58 +0700

    Returning Child Resources

---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,41 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers;
+
+[Route("api/cities/{cityId}/pointsofinterest")]
+[ApiController]
+public class PointsOfInterestController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+    {
+        var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+        if (city == null)
+        {
+            return NotFound("City does not exist for the Id: " + cityId);
+        }
+
+        return Ok(city.PointsOfInterest);
+    }
+
+    [HttpGet("{pointOfInterestId}")]
+    public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId)
+    {
+        var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+        if (city == null)
+        {
+            return NotFound("City does not exist for cityId: " + cityId);
+        }
+
+        var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id == pointOfInterestId);
+        if (pointOfInterest == null)
+        {
+            return NotFound("Point of interest doest not exist for the Id: " + pointOfInterestId);
+        }
+
+        return Ok(pointOfInterest);
+    }
+}

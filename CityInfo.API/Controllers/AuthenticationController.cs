4bf8eba2d4c939df14742f0295d39db47e66213e
Myo Thet <mr.myothet@gmail.com>
2024-11-06 15:14:55 +0700

    Create JwtSecurityToken

---
+++ CityInfo.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,96 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public class AuthenticationRequestBody
+        {
+            public string? UserName { get; set; }
+            public string? Password { get; set; }
+        }
+
+        public class User
+        {
+            public int UserId { get; set; }
+            public string UserName { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string City { get; set; }
+
+            public User(int userId, string userName, string firstName, string lastName, string city)
+            {
+                UserId = userId;
+                UserName = userName;
+                FirstName = firstName;
+                LastName = lastName;
+                City = city;
+            }
+        }
+
+        public AuthenticationController(IConfiguration configuration)
+        {
+            _configuration =
+                configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        [HttpPost("authenticate")]
+        public ActionResult<string> Authenticate(AuthenticationRequestBody request)
+        {
+            // Step 1: validate the username/password
+            var user = ValidateUserCredentials(request.UserName, request.Password);
+
+            if (user is null)
+            {
+                return Unauthorized();
+            }
+
+            // Step 2: create a token
+            var securityKey = new SymmetricSecurityKey(
+                Convert.FromBase64String(_configuration["Authentication:SecretForKey"]!)
+            );
+
+            var signingCredentials = new SigningCredentials(
+                securityKey,
+                SecurityAlgorithms.HmacSha256
+            );
+
+            // that claims that
+            var claimsForToken = new List<Claim>();
+            claimsForToken.Add(new Claim("sub", user.UserId.ToString()));
+            claimsForToken.Add(new Claim("given_name", user.FirstName));
+            claimsForToken.Add(new Claim("family_name", user.LastName));
+            claimsForToken.Add(new Claim("city", user.City));
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                _configuration["Authentication:Issuer"],
+                _configuration["Authentication:Audience"],
+                claimsForToken,
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddHours(1),
+                signingCredentials
+            );
+
+            var tokenToReturn = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+            return Ok(tokenToReturn);
+        }
+
+        private User ValidateUserCredentials(string? userName, string? password)
+        {
+            // we don't have a user DB or table, if you have, check the passed-through
+            // username/password against what's stored in the database.
+
+            // return a new User (values would normally come from user table)
+            return new User(1, userName ?? "", "Kevin", "Dockx", "Antwerp");
+        }
+    }
+}

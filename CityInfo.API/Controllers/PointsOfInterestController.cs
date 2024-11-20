0aac25254afc858f078343f19a6c25a4c5f6dfd3
Myo Thet <mr.myothet@gmail.com>
2024-11-20 21:40:25 +0700

    Creating a Resource, Validating Input and Updating a Resource - full update

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -7,7 +7,7 @@ namespace CityInfo.API.Controllers;
 
 [Route("api/cities/{cityId}/pointsofinterest")]
 [ApiController]
-[Authorize]
+//[Authorize]
 public class PointsOfInterestController : ControllerBase
 {
     [HttpGet]
@@ -23,7 +23,7 @@ public class PointsOfInterestController : ControllerBase
         return Ok(city.PointsOfInterest);
     }
 
-    [HttpGet("{pointOfInterestId}")]
+    [HttpGet("{pointOfInterestId}", Name = "GetPointOfInterest")]
     public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId)
     {
         var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
@@ -35,9 +35,70 @@ public class PointsOfInterestController : ControllerBase
         var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id == pointOfInterestId);
         if (pointOfInterest == null)
         {
-            return NotFound("Point of interest doest not exist for the Id: " + pointOfInterestId);
+            return NotFound("Point of interest does not exist for the Id: " + pointOfInterestId);
         }
 
         return Ok(pointOfInterest);
+    }
+
+    [HttpPost]
+    public ActionResult<PointOfInterestDto> CreatePointOfInterest(
+        int cityId,
+        PointOfInterestForCreationDto pointOfInterest
+    )
+    {
+        var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+        if (city is null)
+        {
+            return NotFound();
+        }
+
+        var maxPointOfInterestId = CitiesDataStore
+            .Current.Cities.SelectMany(c => c.PointsOfInterest)
+            .Max(p => p.Id);
+
+        var finalPointOfInterest = new PointOfInterestDto()
+        {
+            Id = ++maxPointOfInterestId,
+            Name = pointOfInterest.Name,
+            Description = pointOfInterest.Description
+        };
+
+        city.PointsOfInterest.Add(finalPointOfInterest);
+
+        return CreatedAtRoute(
+            "GetPointOfInterest",
+            new { cityId = cityId, pointOfInterestId = finalPointOfInterest.Id },
+            finalPointOfInterest
+        );
+    }
+
+    [HttpPut("{pointofinterestid}")]
+    public ActionResult UpdatePointOfInterest(
+        int cityId,
+        int pointOfInterestId,
+        PointOfInterestForUpdateDto pointOfInterest
+    )
+    {
+        var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+        if (city is null)
+        {
+            return NotFound();
+        }
+
+        // find point of interest
+        var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(p =>
+            p.Id == pointOfInterestId
+        );
+
+        if (pointOfInterestFromStore is null)
+        {
+            return NotFound();
+        }
+
+        pointOfInterestFromStore.Name = pointOfInterest.Name;
+        pointOfInterestFromStore.Description = pointOfInterest.Description;
+
+        return NoContent();
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-### ASP.NET Core Web API Fundamentals
+# ASP.NET Core Web API Fundamentals 
+
+- [x] Getting Acquainted with ASP.NET Core 
+- [x] Creating the API and Returning Resources
+- [ ] Manipulating Resources and Validating Input
+- [ ] Working with Services and Dependency Injection 
+- [ ] Getting Acquainted with Entity Framework Core
+- [ ] Using Entity Framework Core in Controllers
+- [ ] Searching, Filtering and Paging Resources
+- [ ] Securing API
+- [ ] Versioning and Documenting API
+- [ ] Testing and Deploying API
+
+### Manipulating Resources and Validating Input
+- FromQuery - Query string parameters
+- FromForm - IFormFile and IFormFileCollection
+- FromRoute - Route data from the current request
+- FromServices - The services injected as action parameter
+- AsParameters - Method parameters
